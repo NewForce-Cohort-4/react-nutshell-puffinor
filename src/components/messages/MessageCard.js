3f7be10108ad100377f22b6aef5adc8ddb54e3e0
Chance Lane <lanecw@gmail.com>
2021-03-14 04:25:23 -0400

    chat edit working. Needs style and user verification"

--- src/components/messages/MessageCard.js
+++ src/components/messages/MessageCard.js
@@ -2,13 +2,21 @@ import React from "react"
 import "./Messages.css"
 import {Link} from "react-router-dom"
 
-export const MessageCard = ({ message, userName }) => (
+import { MessageList } from "./MessageList"
+
+export const MessageCard = ({ message, userName, id, changeParentState }) => (
     
     <>
         <div className="container-card container card  rounded ">
             <div className='row '>
                 <div className='name col-1  text-nowrap'><h6 className=' fw-bolder'>{userName}</h6></div>
                 <div className='col-2 ><small class = text-muted'>{message.time}</div>
+                <div className='col-2 ><small class = text-muted'>{id}</div>
+                <button 
+                        onClick={changeParentState}
+                        >
+                        Edit
+                </button>
             </div>
             <div >
                 <div >{message.message}</div>
