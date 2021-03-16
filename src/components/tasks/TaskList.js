47a10a66395973731b816419edb681cacfa733b3
MonroeMegalonyx <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-03-15 23:00:48 -0400

    Completed ticket for editing tasks

--- src/components/tasks/TaskList.js
+++ src/components/tasks/TaskList.js
@@ -4,7 +4,7 @@
   Written by Vincent O
 */
 import React, {useContext, useEffect, useState } from "react"
-import { useHistory } from "react-router-dom"
+//import { useHistory } from "react-router-dom"
 import { TaskContext } from "./TaskDataProvider"
 
 export const TasksList = () => {
@@ -13,7 +13,7 @@ export const TasksList = () => {
     functions that a parent provider component exposes.
 
   */
-  const { getTasks, tasks, updateTask } = useContext(TaskContext)
+  const { getTasks, tasks } = useContext(TaskContext)
   
   /*
     useEffect() allows for getting the data from somewhere else.
@@ -28,11 +28,16 @@ export const TasksList = () => {
     Set a form display state to change when the add task form is rendered.
   */
   let [displayForm, setDisplayForm] = useState(false);
+  
+  /*
+    Set an edit display state to change when the add task form is rendered.
+  */
+    let [displayEditID, setDisplayEditID] = useState(0);
 
   /*
     useHistory is used to change the URL, triggering changes to the DOM
   */
-  const history = useHistory()
+  //const history = useHistory()
   
   /*
     Using a ternary operator (way to write a condition in a return) to display the 
@@ -40,6 +45,8 @@ export const TasksList = () => {
     default of false to true when the button is clicked to add a task.
     The conditional keeps the list of tasks open, and swaps the Add button for 
     the add form where user can input new tasks.
+
+    Using a secondary ternary operator to dispaly the edit form inline for each task
   */
     return (
       displayForm ?
@@ -51,9 +58,25 @@ export const TasksList = () => {
       <div className="tasks-container">
         {
           tasks.filter(task => {
+            if (task.userId === localStorage.getItem("nutshell_user")){return task.userId}
+          })
+          .filter(task => {
             return !task.completed
-          }).map(task => {
-            return <TaskItem key={task.id} task={task} />
+          })
+          .sort((a,b) => {
+            return new Date (a.date) - new Date (b.date)
+          })
+          .map(task => {
+            return (
+              displayEditID===task.id ?
+              <>
+              <TaskForm changeparentstate={setDisplayForm} taskstateID={displayEditID} changeparentID={setDisplayEditID} />
+              </>
+              :
+              <>
+              <TaskItem key={task.id} task={task} changeparentstate={setDisplayEditID}/>
+              </>
+            )
           })
         }
       </div>
@@ -67,9 +90,25 @@ export const TasksList = () => {
       <div className="tasks-container">
         {
           tasks.filter(task => {
+            if (task.userId === localStorage.getItem("nutshell_user")){return task.userId}
+          })
+          .filter(task => {
             return !task.completed
-          }).map(task => {
-            return <TaskItem key={task.id} task={task} />
+          })
+          .sort((a,b) => {
+            return new Date (a.date) - new Date (b.date)
+          })
+          .map(task => {
+            return (
+              displayEditID===task.id ?
+              <>
+              <TaskForm changeparentstate={setDisplayForm} taskstateID={displayEditID} changeparentID={setDisplayEditID}/>
+              </>
+              :
+              <>
+              <TaskItem key={task.id} task={task} changeparentstate={setDisplayEditID}/>
+              </>
+            )
           })
         }
       </div>
@@ -81,14 +120,19 @@ export const TasksList = () => {
   Function to print a task in the list and mark tasks completed if checked. To change the appearance and function
   of the tasks edit the HTML here as needed. 
 */
-const TaskItem = ({ task }) => {
+const TaskItem = ({ task, changeparentstate }) => {
   const {updateTask, getTasks} = useContext(TaskContext)
-  
+
   const handleCheckBox = (idOfCurrentTask) => {
     console.log("Checked box")
     updateTask(idOfCurrentTask)
     .then(getTasks)
   }
+
+  const handleClickEditTask = (idOfCurrentTask) => {
+    console.log("You clickd edit for task",task.id)
+    changeparentstate(idOfCurrentTask)
+  };
 
   return(
   <section className="task">
@@ -97,6 +141,7 @@ const TaskItem = ({ task }) => {
     <div className = "checkbox">
       <p>Completed?<input type="checkbox" className="checkboxflag" id={task.id} onClick={()=>handleCheckBox(task.id)}></input></p>
     </div>
+    <button id="saveTask" onClick={() => handleClickEditTask(task.id)}>Edit Task</button>
   </section>
   )
 }
@@ -104,9 +149,10 @@ const TaskItem = ({ task }) => {
 /*
   Function to add a task.
 */
-const TaskForm = ({changeparentstate}) => {
-  console.log("You clicked the button");
-  const { addTask, getTasks } = useContext(TaskContext);
+const TaskForm = ({changeparentstate, taskstateID, changeparentID}) => {
+  console.log("Editing task #",taskstateID);
+  const { addTask, getTasks, editTask } = useContext(TaskContext);
+  
   /*
   With React, we do not target the DOM with `document.querySelector()`. Instead, our return (render) reacts to state or props.
 
@@ -119,32 +165,49 @@ const TaskForm = ({changeparentstate}) => {
     userId: localStorage.getItem("nutshell_user"),
   });
 
-  const history = useHistory();
+  //wait for data before button is active
+  //const [isLoading, setIsLoading] = useState(true);
+
+  //const history = useHistory();
 
   /*
   When a field changes, update state. The return will re-render and display based on the values in state
 */
   const handleControlledInputChange = (event) => {
-    const newTask = { ...task };
-    newTask[event.target.id] = event.target.value;
-    setTask(newTask);
+    const inputtedTask = { ...task };
+    inputtedTask[event.target.id] = event.target.value;
+    setTask(inputtedTask);
   };
 
-  const handleClickSaveTask = () => {
-    console.log("You clickd save")
-    const newTask = { ...task };
+  const handleClickSaveTask = (taskID) => {
+    console.log("You clickd save for task id =",taskID)
+    console.log(task)
+    const inputtedTask = { ...task };
 
-    addTask(newTask)
-    .then(getTasks)
-    .then(() => changeparentstate(false))
+    //setIsLoading(true);
+    if (taskID) {
+      //add the task ID to the state if the task is being edited
+      inputtedTask.id = taskID;
+      editTask(inputtedTask)
+      .then(getTasks)
+      .then(changeparentID(0))
+    }else {
+      addTask(inputtedTask)
+        .then(getTasks)
+        .then(() => changeparentstate(false))
+    }
   };
 
   return (
     <fieldset>
     <input type="date" id = "date" onChange={handleControlledInputChange}/>
     <input type="text" placeholder="Enter task here" id="task" onChange={handleControlledInputChange}/>
-    <button id="saveTask" onClick={() => handleClickSaveTask()}>Save Task</button>
+    <button id="saveTask" 
+          onClick={event => {
+            event.preventDefault() // Prevent browser from submitting the form and refreshing the page
+            handleClickSaveTask(taskstateID)
+          }}>
+        {taskstateID ? <>Save Your Changes</> : <>Add a Task</>}</button>
     </fieldset>
   );
 };
-
